ba4d3752aef9de1780defd06c0a8f65505026843
liyuhang <2484691386@qq.com>
2019-01-23 19:55:31 +0800

    ..

--- git-test/src/com/liyuhang/git/FirstClass.java
+++ git-test/src/com/liyuhang/git/FirstClass.java
@@ -4,4 +4,8 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello");
     }
+
+    public void test(){
+        System.out.println("修改");
+    }
 }
